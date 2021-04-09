e9e8af3fccd0bffd606cd16618973f3efc4370ea
BiaoWu1 <45259332+BiaoWu1@users.noreply.github.com>
2021-04-09 10:29:03 +0800

    Update model_resnet.py

--- Monodepth2/BA2M/networks/model_resnet.py
+++ Monodepth2/BA2M/networks/model_resnet.py
@@ -4,23 +4,31 @@ import torch.utils.model_zoo as model_zoo
 import torch.nn.functional as F
 import torch
 import numpy as np
-from torch.nn import init
-from .cbam import *
-from .bam import *
-
+from .SA2M import *
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152']
+
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
 
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
 
+
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, use_cbam=False):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, use_SA2M = True):
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -29,12 +37,10 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
-
-        if use_cbam:
-            self.cbam = CBAM( planes, 16 )
+        if use_SA2M:
+            self.SA2M = SA2M(planes, 16)
         else:
-            self.cbam = None
-
+            self.SA2M = None
     def forward(self, x):
         residual = x
 
@@ -44,22 +50,22 @@ class BasicBlock(nn.Module):
 
         out = self.conv2(out)
         out = self.bn2(out)
-
         if self.downsample is not None:
             residual = self.downsample(x)
 
-        if not self.cbam is None:
-            out = self.cbam(out)
+        if self.SA2M is not None:
+            out = self.SA2M(out)
 
         out += residual
         out = self.relu(out)
 
         return out
 
+
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, use_cbam=False):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, use_SA2M = True):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -71,12 +77,10 @@ class Bottleneck(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
-
-        if use_cbam:
-            self.cbam = CBAM( planes * 4, 16 )
+        if not use_SA2M:
+            self.SA2M = SA2M(planes * 4, 16)
         else:
-            self.cbam = None
-
+            self.SA2M = None
     def forward(self, x):
         residual = x
 
@@ -94,16 +98,18 @@ class Bottleneck(nn.Module):
         if self.downsample is not None:
             residual = self.downsample(x)
 
-        if not self.cbam is None:
-            out = self.cbam(out)
+        if not self.SA2M is not None:
+            out = self.SA2M(out)
 
         out += residual
         out = self.relu(out)
 
         return out
 
+
 class ResNet(nn.Module):
-    def __init__(self, block, layers,  att_type=None):
+
+    def __init__(self, block, layers, num_classes=1000, use_SA2M = True):
         self.inplanes = 64
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
@@ -111,18 +117,12 @@ class ResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        if att_type=='BAM':
-            self.bam1 = BAM(64*block.expansion)
-            self.bam2 = BAM(128*block.expansion)
-            self.bam3 = BAM(256*block.expansion)
-        else:
-            self.bam1, self.bam2, self.bam3 = None, None, None
-        self.layer1 = self._make_layer(block, 64,  layers[0], att_type=att_type)
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, att_type=att_type)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, att_type=att_type)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, att_type=att_type)
-
+        self.layer1 = self._make_layer(block, 64, layers[0], use_SA2M = True)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, use_SA2M = True)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, use_SA2M = True)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, use_SA2M = True)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -132,7 +132,7 @@ class ResNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def _make_layer(self, block, planes, blocks, stride=1, att_type=None):
+    def _make_layer(self, block, planes, blocks, stride=1, use_SA2M = True):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
@@ -142,10 +142,10 @@ class ResNet(nn.Module):
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, use_cbam=att_type=='CBAM'))
+        layers.append(block(self.inplanes, planes, stride, downsample, use_SA2M = True))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes, use_cbam=att_type=='CBAM'))
+            layers.append(block(self.inplanes, planes, use_SA2M = True))
 
         return nn.Sequential(*layers)
 
@@ -156,64 +156,66 @@ class ResNet(nn.Module):
         x = self.maxpool(x)
 
         x = self.layer1(x)
-        if not self.bam1 is None:
-            x = self.bam1(x)
-
         x = self.layer2(x)
-        if not self.bam2 is None:
-            x = self.bam2(x)
-
         x = self.layer3(x)
-        if not self.bam3 is None:
-            x = self.bam3(x)
-
         x = self.layer4(x)
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
+
         return x
 
-def resnet18(att_type=None, **kwargs):
+def resnet18(pretrained=False, **kwargs):
     """Constructs a ResNet-18 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = ResNet(BasicBlock, [2, 2, 2, 2], att_type, **kwargs)
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
     return model
 
 
-def resnet34(att_type=None, **kwargs):
+def resnet34(pretrained=False, **kwargs):
     """Constructs a ResNet-34 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
     model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
     return model
 
 
-def resnet50(att_type=None, **kwargs):
+def resnet50(pretrained=False, **kwargs):
     """Constructs a ResNet-50 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
     model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet50'], 'pretrained_model/encoder'))
     return model
 
 
-def resnet101(att_type=None, **kwargs):
+def resnet101(pretrained=False, **kwargs):
     """Constructs a ResNet-101 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
     model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
     return model
 
 
-def resnet152(att_type=None, **kwargs):
+def resnet152(pretrained=False, **kwargs):
     """Constructs a ResNet-152 model.
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
     model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
     return model
